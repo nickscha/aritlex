71cf41cec12e30e425c3ace86866b5a84f930d41
nickscha <nick@scharnbeck.de>
2025-10-04 11:54:44 +0200

    [VERSION] bump to v0.2

--- examples/aritlex_win32_nostdlib.c
+++ examples/aritlex_win32_nostdlib.c
@@ -1,4 +1,4 @@
-/* aritlex.h - v0.1 - public domain data structures - nickscha 2025
+/* aritlex.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) Arithmetic Lexer (ARITLEX).
 
